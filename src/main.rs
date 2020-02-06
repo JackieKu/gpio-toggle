1bea1388fe9b2c75cfcdc26cfaea74ef38de72a3
Kai-Chieh Ku <kjackie@gmail.com>
2020-02-06 08:03:09 +0000

    Implement GPIO line control

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,86 @@
 extern crate actix_web;
 
 use actix_files;
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix_web::{middleware, App, HttpRequest, HttpResponse, HttpServer};
+
+extern crate gpio_cdev;
+
+use gpio_cdev::*;
+
+use once_cell::sync::Lazy;
+
+static CONSUMER: &str = "gpio-toggle-web";
 
 trait Port {
     fn get(&self) -> bool;
-    fn set(&self, on: bool);
+    fn set(&mut self, on: bool);
 }
 
 struct MockPort {
+    state: bool,
 }
-static mut MOCK_STATE: bool = false;
 impl Port for MockPort {
     fn get(&self) -> bool {
-        unsafe { MOCK_STATE }
+        self.state
     }
 
-    fn set(&self, on: bool) {
-        unsafe {
-            MOCK_STATE = on;
-        }
+    fn set(&mut self, on: bool) {
+        self.state = on
     }
 }
 
-static PORT: MockPort = MockPort {};
+struct GPIO {
+    handle: LineHandle,
+}
+impl GPIO {
+    fn new(chip: String, line: u32) -> Result<GPIO, errors::Error>  {
+        let line = Chip::new(chip)?
+            .get_line(line)?;
+        let old_value = line.request(LineRequestFlags::INPUT, 0, CONSUMER)?
+            .get_value()?;
+
+        Ok(GPIO {
+            handle: line.request(LineRequestFlags::OUTPUT, old_value, CONSUMER)?
+        })
+    }
+}
+impl Port for GPIO {
+    fn get(&self) -> bool {
+        self.handle.get_value().unwrap() != 0
+    }
+
+    fn set(&mut self, on: bool) {
+        self.handle.set_value(on as u8).unwrap();
+    }
+}
+
+//static mut PORT: Lazy<MockPort> = Lazy::new(|| {
+//    MockPort {state: false}
+//});
+static mut PORT: Lazy<GPIO> = Lazy::new(|| {
+    GPIO::new(String::from("/dev/gpiochip0"), 0).unwrap()
+});
+
+fn unsafe_port() -> &'static mut Lazy<GPIO> {
+    unsafe { &mut PORT }
+}
 
 #[get("/line")]
 async fn get_line(_req: HttpRequest) -> &'static str {  
-    if PORT.get() { "1" } else { "0" }
+    if unsafe_port().get() { "1" } else { "0" }
 }
 
 #[put("/line/{on}")]
-async fn put_line(req: HttpRequest) -> &'static str {
+async fn put_line(req: HttpRequest) -> HttpResponse {
     let new_state = match req.match_info().query("on") {
         "0" => false,
         "1" => true,
-        _ => return "ERROR"
+        _ => return HttpResponse::BadRequest().finish()
     };
 
-    PORT.set(new_state);
+    unsafe_port().set(new_state);
 
-    "OK"
+    HttpResponse::Ok().finish()
 }
 
 #[actix_rt::main]
